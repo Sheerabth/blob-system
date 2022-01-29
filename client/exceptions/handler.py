560b649bdf926334c6c6fce2ebb0b1af4ddb2753
Sheerabth <sheerabth1610@gmail.com>
2022-01-29 10:10:31 +0530

    added: client exceptions

---
+++ client/exceptions/handler.py
@@ -0,0 +1,49 @@
+import typer
+from client.exceptions.base import Error
+from client.exceptions.file import TokenFileNotFound, AccessTokenFileNotFound, RefreshTokenFileNotFound
+from client.exceptions.api import APIException, BadRequestAPIException, TokenExpiredException, UnauthorizedAPIException, ForbiddenAPIException, NotFoundAPIException
+from functools import wraps
+
+
+def exception_handler(api_function):
+
+    @wraps(api_function)
+    def inner(*args, **kwargs):
+
+        try:
+            api_function(*args, **kwargs)
+
+        except AccessTokenFileNotFound:
+            typer.echo("Access token file not found")
+
+        except RefreshTokenFileNotFound:
+            typer.echo("Refresh token file not found")
+
+        except TokenFileNotFound:
+            typer.echo("Token file not found")
+
+        except BadRequestAPIException:
+            typer.echo("Bad request")
+
+        except TokenExpiredException:
+            typer.echo("Token expired")
+
+        except UnauthorizedAPIException:
+            typer.echo("Invalid credentials")
+
+        except ForbiddenAPIException:
+            typer.echo("Request Forbidden")
+
+        except NotFoundAPIException:
+            typer.echo("Not found")
+
+        except APIException:
+            typer.echo("API exception")
+
+        except Error:
+            typer.echo("Base exception")
+
+        raise typer.Exit(code=1)
+
+    return inner
+
