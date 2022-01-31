1a35028243be0d589a3bb8257bb1c9a3ffeb9172
Sheerabth <sheerabth1610@gmail.com>
2022-01-31 18:21:49 +0530

    modified: added rotating file logging

--- server/src/__main__.py
+++ server/src/__main__.py
@@ -1,7 +1,10 @@
 from src.main import app
 import uvicorn
-import logging
+import logging.handlers
 
 if __name__ == "__main__":
-    logging.basicConfig(format="%(levelname)s: %(filename)s[%(lineno)s] - %(message)s", level=logging.INFO)
+    handler = logging.handlers.RotatingFileHandler("server.log", maxBytes=4194304)
+    logging.basicConfig(
+        handlers=[handler], format="%(levelname)s: %(filename)s[%(lineno)s] - %(message)s", level=logging.INFO
+    )
     uvicorn.run(app, host="0.0.0.0", port=8080, log_config=None)
